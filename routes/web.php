ff9300f8ce988da99cc8a06ab23d761b2935ea06
rhoii <rhoydelm@gmail.com>
2025-03-26 19:54:54 +0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,13 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->patch('/users/{id}', 'UserController@update'); // Partial update
     $router->delete('/users/{id}', 'UserController@delete'); // Delete user
 });
+
+// New Unsecured Routes without /api prefix (new addition)
+$router->group([], function () use ($router) {
+    $router->get('/users', 'UserController@index'); // Get all users (http://localhost:8000/users)
+    $router->get('/users/{id}', 'UserController@show'); // Get user by ID (http://localhost:8000/users/{id})
+    $router->post('/users', 'UserController@add'); // Create a new user
+    $router->put('/users/{id}', 'UserController@update'); // Update user
+    $router->patch('/users/{id}', 'UserController@update'); // Partial user update
+    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
+});
