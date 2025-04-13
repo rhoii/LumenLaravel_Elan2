8965e8ba77253e8f40c472566c4adff2e15167fe
rhoii <rhoydelm@gmail.com>
2025-04-13 12:35:00 +0800

    updtes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\UserJob;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
@@ -45,10 +46,14 @@ class UserController extends Controller
         'username' => 'required|max:20',
         'password' => 'required|max:20',
         'gender' => 'required|in:Male,Female',
+        'jobid' => 'required|numeric|min:1|not_in:0',
     ];
 
     $this->validate($request, $rules);
+
+    $userjob = UserJob::findOrFail($request->jobid);
     $user = User::create($request->all());
+    
     return $this->successResponse($user, Response::HTTP_CREATED);
 }
 
